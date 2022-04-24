2d1b915707a8d046888256043384b4fc55985b52
CharlesHuangECNU <charles00@hotmail.com>
2022-04-24 13:26:00 +0800

    extract extra frame info from pkt

--- doc/examples/h264tomp4.c
+++ doc/examples/h264tomp4.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "libavformat/avformat.h"
 #include "libavutil/mathematics.h"
 #include "libavutil/bswap.h"
 #include "libavutil/parseutils.h"
+#include "libavutil/avstring.h"
 
 #define SEPARATOR 0xFF
+#define TRAILER_LEN	8
 
 typedef struct FrameInfo {
 	uint32_t frameid;
@@ -14,6 +18,115 @@ typedef struct FrameInfo {
 
 uint64_t start_timestamp = 0;
 uint64_t last_timestamp = 0;
+
+
+static char* getmp4filename(char* filename, char* outfilename)
+{
+	char *file, *ext, tmp[1024];
+
+	*outfilename = 0x00;
+	file = strrchr(filename, '/');
+	if (file) {
+		if ((file - filename + 1) == strlen(filename)) {		
+			return outfilename;
+		}
+		strncpy(outfilename, filename, file - filename);
+	}
+	else {
+		file = filename;
+	}
+	
+	ext = strrchr(file, '.');
+	if (ext) {
+		if (strrchr(ext, '/')) {
+			*outfilename = 0x00;
+			return outfilename;
+		}
+		strncpy(tmp, file, (int)(ext - file));
+	}
+	else {
+		strcpy(tmp, file);
+	}
+	strcat(tmp, ".mp4");
+	return strcat(outfilename, tmp);
+}
+
+static int alldigits(uint8_t *str, const int len)
+{
+	int i, count = 0;
+	for (i = 0; i < len; i++) {
+		if (av_isdigit(str[i])) count++;
+	}
+	return (len == count)?1:0;
+}
+
+static int hasextrainfo(AVPacket *pkt) {
+	uint8_t *src_buf;
+	uint8_t trailer[TRAILER_LEN];
+	int trailercount = 0;
+
+	if (!pkt) return trailercount;
+
+	src_buf = pkt->data + pkt->size - TRAILER_LEN;
+	memcpy(trailer, src_buf, TRAILER_LEN);
+
+	if (alldigits(trailer,TRAILER_LEN)) {
+		trailercount = atoi(trailer);
+	}
+	return trailercount;
+}
+
+static int8_t *getextrainfo(AVPacket *pkt, const int trailercount) {
+	uint8_t *src_buf, *des_buf, *src_prt, *des_prt;
+	int i, len, count;
+
+	if (!pkt) return NULL;
+	if (trailercount <= 0) return NULL;
+
+	len = trailercount + TRAILER_LEN + 2;
+	src_buf = pkt->data + pkt->size - (TRAILER_LEN + 1);
+	if (*src_buf != SEPARATOR) {
+		av_log(NULL,AV_LOG_ERROR,"separate char error.");
+		return NULL;
+	}
+	src_buf = pkt->data + pkt->size - len;
+	if (*src_buf != SEPARATOR) {
+		av_log(NULL,AV_LOG_ERROR,"separate char error.");
+		return NULL;
+	}
+
+	len = trailercount + 2;
+	des_buf = malloc(len);
+	if (!des_buf) return NULL;
+	memset(des_buf, 0, len);
+
+	count = 1;
+	i = 1;
+	des_prt = des_buf;
+	src_prt = src_buf + 1;
+	while (i + 2 < len)
+	{
+		if ((src_prt[0] == 0x00) && (src_prt[1] == 0x00) && (src_prt[2] == 0x03)) {
+			count += 2;
+			memcpy(des_prt, src_buf, count);
+			des_prt += count; 
+			src_prt += 3;
+			src_buf = src_prt;
+			count = 0;
+			i += 3;
+		}
+		else {
+			count += 1;
+			src_prt += 1;
+			i += 1;
+		}
+	}
+	count += (len - i);
+	if (count) {
+		memcpy(des_prt, src_buf, count);
+	}
+	return des_buf;
+}
 
 static void formatdatetime(char *formated_datetime, uint64_t timestamp)
 {
@@ -50,69 +163,9 @@ static void formattime(char *formated_time, uint64_t time, int decimals, enum AV
     sprintf(l_formated_time, "%d.%0*dS", seconds, decimals, fractions);
 }
 
-static struct FrameInfo *get_frameinfo_file(FILE *file, struct FrameInfo *frame_info, const int8_t need_swap)
+static struct FrameInfo *get_frameinfo(uint8_t *buffer, struct FrameInfo *frame_info, const int8_t need_swap)
 {
-	unsigned char separate_char;
-	int len;
-
-	if (frame_info == NULL) return NULL;
-
-	if (file == NULL) {
-		av_log(NULL,AV_LOG_ERROR,"index file pointer is nulll.");
-		return NULL;
-	}
-
-	frame_info->nextfilename = NULL;
-
-	len = fread(&separate_char,sizeof(char),1,file);
-	if (len != 1) {
-		av_log(NULL,AV_LOG_ERROR,"end of index file.");
-		return NULL;
-	}
-	if (separate_char != SEPARATOR) {
-		av_log(NULL,AV_LOG_ERROR,"separate char error.");
-		return NULL;
-	}
-
-	len = fread(&frame_info->frameid,sizeof(uint32_t),1,file);
-	if (len != 1) {
-		av_log(NULL,AV_LOG_ERROR,"end of index file.");
-		return NULL;
-	}
-	if (need_swap) {
-		frame_info->frameid = av_bswap32(frame_info->frameid);
-	}
-	
-	av_log(NULL,AV_LOG_DEBUG,"frame id : %d",frame_info->frameid);
-
-	len = fread(&frame_info->timestamp,sizeof(uint64_t),1,file);
-	if (len != 1) {
-		av_log(NULL,AV_LOG_ERROR,"end of index file.");
-		return NULL;
-	}
-	if (need_swap) {
-		frame_info->timestamp = av_bswap64(frame_info->timestamp);
-	}
-
-	av_log(NULL,AV_LOG_DEBUG,"timestamp : %ld",frame_info->timestamp);
-
-	len = fread(&separate_char,sizeof(char),1,file);
-	if (len != 1) {
-		av_log(NULL,AV_LOG_ERROR,"end of index file.");
-		return NULL;
-	}
-	if (separate_char != SEPARATOR) {
-		av_log(NULL,AV_LOG_ERROR,"separate char error.");
-		return NULL;
-	}
-
-	return frame_info;
-}
-
-static struct FrameInfo *get_frameinfo_buffer(char *buffer, struct FrameInfo *frame_info, const int8_t need_swap)
-{
-	unsigned char separate_char;
-	int len;
+	uint8_t separate_char;
 
 	if (frame_info == NULL) return NULL;
 
@@ -156,21 +209,11 @@ static struct FrameInfo *get_frameinfo_buffer(char *buffer, struct FrameInfo *fr
 	return frame_info;
 }
 
-static int setTimeStamp(AVPacket *pkt, FILE *file, int8_t need_swap)
+static int setTimeStamp(AVPacket *pkt, uint8_t *buffer, int8_t need_swap)
 {
-	struct FrameInfo frame_info, *f_info;
-	char buffer[128];
-	
-	if (file == NULL) {
-		av_log(NULL,AV_LOG_ERROR,"index file pointer is nulll.");
-		return -1;
-	}
+	struct FrameInfo frame_info;
 
-	// if (get_frameinfo_file(file, &frame_info, need_swap) == NULL) return -1;
-
-	if (fread(buffer, 14, 1, file) != 1) 
-		return -1;
-	if (get_frameinfo_buffer(buffer, &frame_info, need_swap) == NULL) 
+	if (get_frameinfo(buffer, &frame_info, need_swap) == NULL) 
 		return -1;
 
 	//Write PTS
@@ -211,35 +254,39 @@ static void close_index(FILE *file)
 
 int main(int argc, char **argv)
 {
-	AVOutputFormat *ofmt = NULL;
 	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
 	AVPacket pkt;
-	const char *in_filename;
+	char in_filename[1024], out_filename[1024], index_filename[1024];
+	char format_time[32];
 	int ret, i;
 	int stream_index = 0;
 	int *stream_mapping = NULL;
 	int stream_mapping_size = 0;
+	int m_frame_index = 0;
+	int trailercount = 0;
+	uint8_t buffer[128];
 
-	const char *index_filename;
 	FILE *index_file = NULL;
- 
-	in_filename  = "./0420/stream_with_pts.h264";
-	// in_filename  = "./stream_without_pts.h264";
- 	// in_filename  = "./rec.h264";
-	// in_filename  = "./timestemp.h264";
+	memset(in_filename, 0, 1024);
+	memset(out_filename, 0, 1024);
+	memset(index_filename, 0, 1024);
+	
+	strcpy(in_filename, "./0420/stream_with_pts.h264");
+	// strcpy(in_filename, "./0420/stream_without_pts.h264");
+	// strcpy(in_filename, "./0420/rec.h264");
+	// strcpy(in_filename, "./0420/timestemp.h264");
 
-	index_filename = "./0420/stream_only_pts.h264";
-	if ((index_file = open_index(index_filename)) == NULL) {
-		printf("Could not open index file : %s.\n", index_filename); 
+	getmp4filename(in_filename, out_filename);
+
+	// strcpy(index_filename, "./0420/stream_only_pts.h264");
+	if (strlen(index_filename) != 0) {
+		if ((index_file = open_index(index_filename)) == NULL) {
+				printf("Could not open index file : %s.\n", index_filename); 
+			}
 	}
- 
-	char cTime[128];
-	char out_filename[128];
-	sprintf(cTime,"%s", "stream_without_pts");
-	sprintf(out_filename,"./0420/%s.mp4",cTime);
- 
+	 
 	if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
-		printf("Could not open input file : %s.\n", out_filename); 
+		printf("Could not open input file : %s.\n", in_filename); 
 		goto end;
 	}
 	if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
@@ -264,9 +311,7 @@ int main(int argc, char **argv)
         ret = AVERROR(ENOMEM);
         goto end;
     }
- 
-	ofmt = ofmt_ctx->oformat;
- 
+  
 	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
 		AVStream *out_stream;
 		AVStream *in_stream = ifmt_ctx->streams[i];
@@ -299,7 +344,7 @@ int main(int argc, char **argv)
 		out_stream->time_base = (AVRational){1, AV_TIME_BASE};
 	}
  
-	if (!(ofmt->flags & AVFMT_NOFILE)) {
+	if (!(ofmt_ctx->oformat->flags & AVFMT_NOFILE)) {
 		ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
 		if (ret < 0) {
 			fprintf(stderr, "Could not open output file '%s'", out_filename);
@@ -313,8 +358,7 @@ int main(int argc, char **argv)
         goto end;
 	}
 
-	int m_frame_index = 0;
-
+	m_frame_index = 0;
 	while (1) {
 		AVStream *in_stream, *out_stream;
  
@@ -335,24 +379,36 @@ int main(int argc, char **argv)
 		//add timestamp
 		if(pkt.pts==AV_NOPTS_VALUE){
 			if (index_file == NULL) {
-				//Write PTS
-				AVRational time_base1=in_stream->time_base;
-				//Duration between 2 frames (us)
-				int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(in_stream->r_frame_rate);
+				
+				if (!(trailercount = hasextrainfo(&pkt))) {
+					//Write PTS
+					AVRational time_base1=in_stream->time_base;
+					//Duration between 2 frames (us)
+					int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(in_stream->r_frame_rate);
 
-				//Parameters
-				pkt.pts=(double)(m_frame_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
-				pkt.dts=pkt.pts;
-				pkt.duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+					//Parameters
+					pkt.pts=(double)(m_frame_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+					pkt.dts=pkt.pts;
+					pkt.duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
 
-				/* copy packet */
-				pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-				pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-				pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-				pkt.pos = -1;
+					/* copy packet */
+					pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+					pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+					pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+					pkt.pos = -1;
+				}
+				else {
+					uint8_t *des_buf = getextrainfo(&pkt, trailercount);
+					if (des_buf) {
+						setTimeStamp(&pkt, des_buf, 1);
+						free(des_buf);
+					}
+				}		
 			}
 			else {
-				setTimeStamp(&pkt, index_file, 1);
+				if (fread(buffer, 14, 1, index_file) != 1) 
+					return -1;
+				setTimeStamp(&pkt, buffer, 1);
 			}
 		}
   
@@ -366,9 +422,8 @@ int main(int argc, char **argv)
 
 	av_write_trailer(ofmt_ctx);
 
-	char format_time[32];
-	
 	printf("\n");
+	printf("Output : %s\n", out_filename);
 	formatdatetime(format_time, start_timestamp);
  	printf("start time : %s, ", format_time);
  	formatdatetime(format_time, last_timestamp);
@@ -379,7 +434,7 @@ int main(int argc, char **argv)
 
 end:
 	avformat_close_input(&ifmt_ctx); 
-	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+	if (ofmt_ctx && !(ofmt_ctx->oformat->flags & AVFMT_NOFILE))
 		avio_closep(&ofmt_ctx->pb);
 	avformat_free_context(ofmt_ctx);
  
